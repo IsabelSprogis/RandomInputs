7f79e353d1647c1aac717b0991f9ac80bcf72d9a
Isabel Cristina Sarti Sprogis <tc.isprogis@padtec.com.br>
2020-11-21 11:07:40 -0300

    readability

--- src/RandomInputsGenerator.java
+++ src/RandomInputsGenerator.java
@@ -61,7 +61,8 @@ public class RandomInputsGenerator {
 		  De modo que, escolhido o programa e os casos de testes possa-se alterar aqui para gerar numeros randomicos 
 		  dentro do limite aceitavel para que o Junit esteja correto e assim o PITEST possa rodar
 		*/
-		int freqAbaixo75, freq75Mais, nota1Baixa, nota1BaixaMenor30, nota1Alta, nota2Baixa, nota2BaixaMenor30, nota2Alta, notaFinalBaixa, notaFinalAlta;
+		int freqAbaixo75, freq75Mais, nota1Baixa, nota1BaixaMenor30, nota1Alta, nota2Baixa, 
+			nota2BaixaMenor30, nota2Alta, notaFinalBaixa, notaFinalAlta;
 		freqAbaixo75 = generateRandomNumberInRange(74,0);
 		writeInFile(freqAbaixo75);
 		
@@ -71,7 +72,7 @@ public class RandomInputsGenerator {
 		nota1Baixa = generateRandomNumberInRange(50,30);
 		writeInFile(nota1Baixa);
 		
-		nota1BaixaMenor30 = generateRandomNumberInRange(30,0);
+		nota1BaixaMenor30 = generateRandomNumberInRange(29,0);
 		writeInFile(nota1BaixaMenor30);
 		
 		nota1Alta = generateRandomNumberInRange(100,70);
@@ -80,18 +81,16 @@ public class RandomInputsGenerator {
 		nota2Baixa = generateRandomNumberInRange(50,30);
 		writeInFile(nota2Baixa);
 		
-		nota2BaixaMenor30 = generateRandomNumberInRange(30,0);
+		nota2BaixaMenor30 = generateRandomNumberInRange(29,0);
 		writeInFile(nota2BaixaMenor30);
 		
 		nota2Alta = generateRandomNumberInRange(100,70);
 		writeInFile(nota2Alta);
 		
-		notaFinalBaixa = generateRandomNumberInRange(50,0);
+		notaFinalBaixa = generateRandomNumberInRange(69,0);
 		writeInFile(notaFinalBaixa);
 		
 		notaFinalAlta = generateRandomNumberInRange(100,70);
 		writeInFile(notaFinalAlta);
-	}
-	
-	
+	}	
 }
